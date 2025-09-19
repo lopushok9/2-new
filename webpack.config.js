ca1af2608907fe9d9e6b40a53e72bc75f641d0f1
lopushok9 <y.g.tsygankov@gmail.com>
2025-09-19 20:52:03 +0300

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/SolanaAuth.jsx',
+  output: {
+    filename: 'solana-auth.bundle.js',
+    path: path.resolve(__dirname, 'public/js'),
+    library: 'SolanaAuth',
+    libraryTarget: 'umd',
+    globalObject: 'this'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  externals: {
+    'react': 'React',
+    'react-dom': 'ReactDOM'
+  }
+};

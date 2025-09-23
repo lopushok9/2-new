d32f76c59c74fdd860084b6822bffe5b695f6e23
lopushok9 <y.g.tsygankov@gmail.com>
2025-09-23 21:24:46 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -26,11 +26,38 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.m?js/,
+        resolve: {
+          fullySpecified: false,
+        },
+      },
     ],
   },
   resolve: {
     extensions: ['.js', '.jsx'],
+    fallback: {
+      "crypto": require.resolve("crypto-browserify"),
+      "stream": require.resolve("stream-browserify"),
+      "assert": require.resolve("assert"),
+      "http": require.resolve("stream-http"),
+      "https": require.resolve("https-browserify"),
+      "os": require.resolve("os-browserify/browser"),
+      "buffer": require.resolve("buffer"),
+      "process": require.resolve("process/browser"),
+      "vm": require.resolve("vm-browserify"),
+    },
   },
+  plugins: [
+    new (require('webpack')).ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  ],
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   devtool: false, // 👈 отключаем eval
+  externals: {
+    'react': 'React',
+    'react-dom': 'ReactDOM'
+  }
 };
